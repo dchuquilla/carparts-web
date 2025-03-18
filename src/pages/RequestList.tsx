993922beb889fbb8fbb7088fbcac376dccf03b5d
Dario Chuquilla <dario.chuquilla@gmail.com>
2025-03-17 23:42:15 -0500

    add loading indicators for requests and images in RequestList

--- src/pages/RequestList.tsx
+++ src/pages/RequestList.tsx
@@ -10,25 +10,37 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
 import axios from 'axios';
-import { Box, Divider, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import { Box, CircularProgress, Divider, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
 import KeyboardDoubleArrowRightTwoToneIcon from '@mui/icons-material/KeyboardDoubleArrowRightTwoTone';
 import { useNavigate } from 'react-router-dom';
 
 const RequestList: React.FC = () => {
   const [requests, setRequests] = useState([]);
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const [loadingImage, setLoadingImage] = useState(true);
 
   useEffect(() => {
     axios.get('https://dev-api.quientiene.com/api/v1/requests')
       .then(response => {
         setRequests(response.data);
+        setLoading(false);
       })
       .catch(error => {
         console.error('There was an error fetching the requests!', error);
+        setLoading(false);
       });
   }, []);
 
-  const handleCardClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>, to: string) => {
+    const handleImageLoad = () => {
+      setLoadingImage(false);
+    };
+
+    if (loading) {
+      return <CircularProgress />;
+    }
+
+  const handleCardClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, to: string) => {
     event.preventDefault();
     setTimeout(() => {
       void navigate(to);
@@ -43,14 +55,17 @@ const RequestList: React.FC = () => {
         gridTemplateColumns: 'repeat(auto-fill, minmax(min(300px, 100%), 1fr))',
       }}>
         {requests.map((request: any) => (
-          <Card key={request.id} sx={{ maxWidth: 345, textDecoration: 'none' }} component="a" href="/"
-            onClick={(event) => handleCardClick(event, "/")}>
+          <Card key={request.id} sx={{ maxWidth: 345, textDecoration: 'none' }}
+            onClick={(event) => handleCardClick(event, `/requests/${request.show_key}`)}>
             <CardActionArea>
+              {loadingImage && <CircularProgress />}
               <CardMedia
                 component="img"
                 height="140"
                 image={request.part_image ? request.part_image : "/quien_tiene_logo_n.png"}
-                alt="green iguana"
+                alt="Part image"
+                onLoad={handleImageLoad}
+                style={{ display: loadingImage ? 'none' : 'block' }}
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
@@ -58,19 +73,19 @@ const RequestList: React.FC = () => {
                 </Typography>
                 <Divider />
                 <List>
-                  <ListItem key={request.id}>
+                  <ListItem>
                     <ListItemIcon>
                       <KeyboardDoubleArrowRightTwoToneIcon sx={[ { minWidth: 0, justifyContent: 'center' } ]} />
                     </ListItemIcon>
                     <ListItemText primary={request.part_brand} secondary='Marca' />
                   </ListItem>
-                  <ListItem key={request.id}>
+                  <ListItem>
                     <ListItemIcon>
                       <KeyboardDoubleArrowRightTwoToneIcon sx={[ { minWidth: 0, justifyContent: 'center' } ]} />
                     </ListItemIcon>
                     <ListItemText primary={request.part_model} secondary='Modelo' />
                   </ListItem>
-                  <ListItem key={request.id}>
+                  <ListItem>
                     <ListItemIcon>
                       <KeyboardDoubleArrowRightTwoToneIcon sx={[ { minWidth: 0, justifyContent: 'center' } ]} />
                     </ListItemIcon>
