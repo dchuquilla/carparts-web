aa24e6ae3cbcc441099fd5a9848bb8155a3c3461
Dario Chuquilla <dario.chuquilla@gmail.com>
2025-04-01 10:20:22 -0500

    fix: remove await from i18n initialization in i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import esTranslation from '..//public/locales/es/translation.json';
 import enTranslation from '..//public/locales/en/translation.json';
 
-await i18n
+i18n
   .use(initReactI18next)
   .init({
     fallbackLng: 'es',
