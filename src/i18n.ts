634b13977eed464898922d4c1c289cb85245244a
Dario Chuquilla <dario.chuquilla@gmail.com>
2025-07-12 22:36:58 -0500

    fix: correct import paths in i18n configuration

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import esTranslation from '..//public/locales/es/translation.json';
-import enTranslation from '..//public/locales/en/translation.json';
+import esTranslation from '../public/locales/es/translation.json';
+import enTranslation from '../public/locales/en/translation.json';
 
 i18n
   .use(initReactI18next)
