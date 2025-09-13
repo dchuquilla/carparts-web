46e565fc77abcdd7ec652347d073bd54c102b326
Dario Chuquilla <dario.chuquilla@gmail.com>
2025-09-13 12:44:00 -0500

    fix: add 'carparts-web.onrender.com' to allowedHosts in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     cors: true,
-    allowedHosts: ['.quientiene.com']
+    allowedHosts: ['.quientiene.com', 'carparts-web.onrender.com']
   },
   define: {
     'process.env': {},
